6bf70a3da915ce347d9a11609059d3eeb94043e5
Minchelin42 <https://github.com/Minchelin42>
2023-04-03 20:42:08 +0900

    SnapKit을 이용한 Auto Layout

--- HelloWorld_Blue/HelloWorld_Blue/ViewController.swift
+++ HelloWorld_Blue/HelloWorld_Blue/ViewController.swift
@@ -6,15 +6,15 @@
 //
 
 import UIKit
+import SnapKit
 
 class ViewController: UIViewController {
     
     var helloworldLabel: UILabel = {
-        let lbl = UILabel(frame: CGRect(x: 0, y: 88, width: UIScreen.main.bounds.width, height: 60))
+        let lbl = UILabel(frame: CGRect(x: 0, y: 0, width: 150, height: 60))
         lbl.text = "Hello World!"
         lbl.font = .systemFont(ofSize: 25, weight: UIFont.Weight.medium)
         lbl.textColor = .white
-        lbl.textAlignment = .center
         return lbl
     }()
     
@@ -22,8 +22,12 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         
         self.view.addSubview(helloworldLabel)
-        helloworldLabel.center = self.view.center
+        self.helloworldLabel.snp.makeConstraints {
+            $0.center.equalTo(self.view)
+        }
         self.view.backgroundColor = UIColor.blue
+        
+        
     }
     
    
